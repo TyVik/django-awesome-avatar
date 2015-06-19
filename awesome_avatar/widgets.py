fac5bb39ff89037d9c8a5b43c87cc61a415e6b3d
SReiver <SReiver@users.noreply.github.com>
2015-06-19 14:58:50 +0300

    Update widgets.py

--- awesome_avatar/widgets.py
+++ awesome_avatar/widgets.py
@@ -43,7 +43,7 @@ class AvatarWidget(FileInput):
         context['name'] = name
         context['config'] = config
 
-        context['avatar_url'] = value.url if value else config.default_image
+        context['avatar_url'] = value.url if value and 'url' in value else config.default_image
         context['id'] = attrs.get('id', 'id_' + name)
         # todo fix HACK
         context['STATIC_URL'] = settings.STATIC_URL
